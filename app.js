55270a81b287c4739d0dea2ebaf75486d32e3476
rkirk430 <r.kirk430@gmail.com>
2022-04-07 12:57:59 -0400

    Added Card Deck with Array of Objects

---
+++ app.js
@@ -0,0 +1,55 @@
+// Card Deck = Array of Objects
+
+let cardDeck = [
+    {
+        name: 'Ace',
+        suite: 'spades',
+        number: '14',
+        image: '',
+    },
+    {
+        name: 'Ace',
+        suite: 'hearts',
+        number: '14',
+        image: '',
+    },
+    {
+        name: 'Ace',
+        suite: 'diamonds',
+        number: '14',
+        image: '',
+    },
+    {
+        name: 'Ace',
+        suite: 'clubs',
+        number: '14',
+        image: '',
+    },
+    {
+        name: 'two',
+        suite: 'spades',
+        number: '2',
+        image: '',
+    },
+    {
+        name: 'two',
+        suite: 'hearts',
+        number: '2',
+        image: '',
+    },
+    {
+        name: 'two',
+        suite: 'diamonds',
+        number: '2',
+        image: '',
+    },
+    {
+        name: 'two',
+        suite: 'clubs',
+        number: '2',
+        image: '',
+    },
+]
+
+
+
