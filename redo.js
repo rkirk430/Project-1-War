351a070392eac1264142f8cc8640d5be7e9e8645
rkirk430 <r.kirk430@gmail.com>
2022-04-08 19:39:18 -0400

    Shuffled Deck Working in console

--- redo.js
+++ redo.js
@@ -17,33 +17,47 @@ function getCardDeck () {
 
 let fullDeck = getCardDeck()  // Converting function to an object
 
-console.log(fullDeck)
+console.log(fullDeck)  //Logs full card deck
 
-console.log(typeof(fullDeck));
-
-
+console.log(typeof(fullDeck));  //Logs Object
 
 
 
-function shuffle (cardDeck) {
+function shuffle (fullDeck) {
     for(let i = 0; i < 500; i++){
-        let location1 = Math.floor((Math.random()* cardDeck.length));
-        let location2 = Math.floor((Math.random() * cardDeck.length));
-        let tmp = cardDeck[location1];
+        let location1 = Math.floor((Math.random()* fullDeck.length));
+        let location2 = Math.floor((Math.random() * fullDeck.length));
+        let tmp = fullDeck[location1];
 
-        cardDeck[location1] = cardDeck[location2];
-        cardDeck[location2] = tmp
+        fullDeck[location1] = fullDeck[location2];
+        fullDeck[location2] = tmp
     }
 }
 
-function shuffledDeck(cardDeck) {
-    document.getElementById("cardDeck").innerHTML = "";
-    for (let i = 0; i < cardDeck.length; i++) {
-        let card = document.createElement("div");
-        let value = document.createElement("div");
-        let suit = document.createElement("div");
-    }
-}
+let shuffledDeck = shuffle(fullDeck);
+
+console.log(shuffledDeck); // why is this coming back as undefined
+
+console.log(typeof(shuffledDeck));  //Undefined
+
+// function shuffledDeck(cardDeck) {
+//     document.getElementById("cardDeck").innerHTML = "";
+//     for (let i = 0; i < cardDeck.length; i++) {
+//         let card = document.createElement("div");
+//         let value = document.createElement("div");
+//         let suit = document.createElement("div");
+
+//         card.className = "card";
+//         value.className = "value";
+//         suit.className = "suit" + cardDeck[i].Suit;
+
+//         value.innerHTML = cardDeck[i].Value;
+//         card.appendChild(value);
+//         card.appendChild(suit);
+
+//         document.getElementById("deck").appendChild(card);
+//     }
+// }
 
 
 
